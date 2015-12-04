2533b2a02779183084fb409cd5972f4bdd9d5442
jayjaysilva <jethrojundelossantos@gmail.com>
2015-12-03 22:56:31 -0500

    Expansys using crawl(but not all items. idunno why)

--- expansys/expansys/spiders/www_expansys_com_sg_crawler.py
+++ expansys/expansys/spiders/www_expansys_com_sg_crawler.py
@@ -1,36 +1,42 @@
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from ..items import ExpansysItem
-from ..pipelines import ExpansysPipeline
 from datetime import datetime
+import re
 
 class ExpansysSpider(CrawlSpider):
-
+    ctr=0
     name = 'expansys'
     allowed_domains = ['expansys.com.sg']
-    start_urls = ['http://www.expansys.com.sg/mobile-phones']
-    #rules = [Rule(LxmlLinkExtractor(allow=''),callback='parse_item',follow=True)]    
-    
-    rules = (Rule(LxmlLinkExtractor(allow=(''), restrict_xpaths=('//a[@class="next"]',)), callback="parse_items", follow= True),)
-    
+    start_urls = ['http://www.expansys.com.sg/']
+    rules = [Rule(LxmlLinkExtractor(allow=r'.+/\S+-\d+/'),callback='parse_items',follow=True)]    
+
     item = ExpansysItem()
     
-    pipeline = set([
-        ExpansysPipeline
-    ])
-    
     def parse_items(self, response):
-        hxs = HtmlXPathSelector(response)
         items = []
-        jayjay="wewewe"
         item = ExpansysItem()
-        for r in response.xpath('//html'):
-            item['title'] = r.xpath('//h1[@itemprop="name"]/text()').extract()
-            item['urls'] =  r.xpath('//link/@href')[0].extract()
-            item['price'] = r.xpath('//span[@itemprop="price"]/text()').extract()
-            #item['currency'] = r.xpath('//p[@id="price"]/meta/@content').extract()
-            item['sku']=r.xpath('//ul[@class="product-sku"]/li/span/@content').extract()
-            #item['category'] =r.xpath('//ul[@id="breadcrumbs"]//span/text()').extract()
+        for r in response.xpath('//div[@id="prod_core"]'):
+            item['title'] = r.xpath('//h1[@itemprop="name"]/text()').extract() or None
+            item['urls'] =  response.url or None
+            item['price'] = r.xpath('//p[@id="price"]/strong/span/text()').extract()
+            checkmate = r.xpath('//ul[@class="product-sku"]/li').extract()
+            for c in checkmate:
+              sku = re.search(r'sku:(\w+)',str(c))
+              ean = re.search(r'ean:(\w+)',str(c))
+              upc = re.search(r'upc:(\w+)',str(c))
+              mfr = re.search(r'mpn:(\w+)',str(c))
+              brand = re.search(r'"brand">(\w+)',str(c))
+              if sku:
+                item['sku'] = sku.group(1)
+              if ean:
+                item['ean'] = ean.group(1)
+              if upc:
+                item['upc'] = upc.group(1)
+              if mfr:
+                item['mpn'] = mfr.group(1)
+              if brand:
+                item['brand'] = brand.group(1)
             items.append(item)
         return(items)
