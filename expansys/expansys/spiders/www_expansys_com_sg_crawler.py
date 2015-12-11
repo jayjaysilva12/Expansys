ac80cd2bf31f21bda261e2c71a61ea99e704b52c
jayjaysilva <jethrojundelossantos@gmail.com>
2015-12-10 22:54:24 -0500

    can now scrape all unique items

--- expansys/expansys/spiders/www_expansys_com_sg_crawler.py
+++ expansys/expansys/spiders/www_expansys_com_sg_crawler.py
@@ -1,6 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
-from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.linkextractors import LinkExtractor
 from ..items import ExpansysItem
 from datetime import datetime
 import re
@@ -10,34 +10,42 @@ class ExpansysSpider(CrawlSpider):
     name = 'expansys'
     allowed_domains = ['expansys.com.sg']
     start_urls = ['http://www.expansys.com.sg/']
-    rules = [Rule(LxmlLinkExtractor(allow=r'.+/\S+-\w+/?page=[0-9]+#\S+'),follow=True),
-             Rule(LxmlLinkExtractor(allow=r'.+/\S+-\d+/'),callback='parse_items',follow=True)]
+    rules = [Rule(LinkExtractor(allow=r'.+/.+/',deny=(r'.+/.+\d+/',r'.+.filter.+',r'.+.user.+',r'.+.aspx.+',r'.+signin.+','.+blog.+')),follow=True),
+             Rule(LinkExtractor(allow=r'.+/\S+-\w+/?page=[0-9]+#=listing',deny=(r'.+.filter.+',r'.+.user.+',r'.+.aspx.+',r'.+signin.+','.+blog.+')),follow=True),                    Rule(LinkExtractor( allow = (r'.+/.+\d+/',),deny =(r'.+?filter.+',r'.+forum.+',r'.+signin.+', r'.+aspx.+','.+blog.+')), callback='parse_items',follow = True)]
+    urlList=[]
     #r'.+/\S+-\d+/'
+    #Rule(LxmlLinkExtractor(allow=r'.+/\S+-\w+/?page=[0-9]+#\S+',deny=(r'.+.filter\S+',r'.+.user\S+',r'.+.aspx\S+')),callback='parse_second',follow=True),
+    #Rule(LxmlLinkExtractor(allow=r'.+/\S+-\d+/',deny=(r'.+.filter\S+',r'.+.user\S+',r'.+.aspx\S+')),callback='parse_items',follow=True)
     item = ExpansysItem()
-    
+
     def parse_items(self, response):
+        print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
         items = []
         item = ExpansysItem()
-        for r in response.xpath('//div[@id="prod_core"]'):
-            item['title'] = r.xpath('//h1[@itemprop="name"]/text()').extract() or None
-            item['urls'] =  response.url or None
-            item['price'] = r.xpath('//p[@id="price"]/strong/span/text()').extract()
-            checkmate = r.xpath('//ul[@class="product-sku"]/li').extract()
-            for c in checkmate:
-              sku = re.search(r'sku:(\w+)',str(c))
-              ean = re.search(r'ean:(\w+)',str(c))
-              upc = re.search(r'upc:(\w+)',str(c))
-              mfr = re.search(r'mpn:(\w+)',str(c))
-              brand = re.search(r'"brand">(\w+)',str(c))
-              if sku:
-                item['sku'] = sku.group(1)
-              if ean:
-                item['ean'] = ean.group(1)
-              if upc:
-                item['upc'] = upc.group(1)
-              if mfr:
-                item['mpn'] = mfr.group(1)
-              if brand:
-                item['brand'] = brand.group(1)
-            items.append(item)
-        return(items)
+        urlTest=response.url
+        if urlTest:
+          if urlTest not in self.urlList:
+            self.urlList.append(urlTest)
+            for r in response.xpath('//div[@id="prod_core"]'):
+                item['title'] = r.xpath('//h1[@itemprop="name"]/text()').extract() or None
+                item['urls'] =  response.url or None
+                item['price'] = r.xpath('//p[@id="price"]/strong/span/text()').extract() or None
+                checkmate = r.xpath('//ul[@class="product-sku"]/li').extract() or None
+                for c in checkmate:
+                  sku = re.search(r'sku:(\w+)',str(c))
+                  ean = re.search(r'ean:(\w+)',str(c))
+                  upc = re.search(r'upc:(\w+)',str(c))
+                  mfr = re.search(r'mpn:(\w+)',str(c))
+                  brand = re.search(r'"brand">(\w+)',str(c))
+                  if sku:
+                    item['sku'] = sku.group(1)
+                  if ean:
+                    item['ean'] = ean.group(1)
+                  if upc:
+                    item['upc'] = upc.group(1)
+                  if mfr:
+                    item['mpn'] = mfr.group(1)
+                  if brand:
+                    item['brand'] = brand.group(1)
+                items.append(item)
+            return(items)
